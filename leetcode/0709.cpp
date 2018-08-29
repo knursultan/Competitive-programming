5c1bdcd888820764eb928821b131a788883cabc0
Нурсултан <nursultan@MacBook-Air-Nursultan.local>
2018-08-29 15:22:25 +0600

    0709

---
+++ leetcode/0709.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    string toLowerCase(string str) {
+        for (int i = 0; i < str.length(); i++)
+            if ('A' <= str[i] && str[i] <= 'Z')
+                str[i] = char(str[i] + 'a' - 'A');
+        return str;
+    }
+};
