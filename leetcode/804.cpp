4b60fdfd1371b88796e20d74436734b7c5b65ecf
Нурсултан <nursultan@MacBook-Air-Nursultan.local>
2018-09-03 14:46:42 +0600

    804

---
+++ leetcode/804.cpp
@@ -0,0 +1,33 @@
+/*
+	To solve this problem
+	1) write Morse code alphabet in vector
+	2) traverse the words array and each traverse each item
+	3) convert letter to code
+	4) create morse code string & add to map
+	5) print the map size
+
+*/
+
+
+class Solution {
+public:
+    vector<string> alphabet = {".-", "-...", "-.-.", "-..", ".", 
+                           "..-.", "--.", "....", "..", ".---", 
+                           "-.-", ".-..", "--", "-.", "---", ".--.", 
+                           "--.-", ".-.", "...", "-", "..-", "...-", 
+                           ".--", "-..-", "-.--", "--.."};
+
+    map <string, int> ans;
+    
+    int uniqueMorseRepresentations(vector<string>& words) {
+        for (int i = 0; i < words.size(); i++) {
+            string tmp = "";
+            for (int j = 0; j < words[i].size(); j++) {
+                tmp += alphabet[words[i][j] - 'a'];
+            }
+            ans[tmp] = ans.find(tmp) == ans.end() ? 1 : ++ans[tmp];
+        }
+        return ans.size();
+    }
+};
+
