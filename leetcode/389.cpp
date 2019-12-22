6bf42a9ec8dba4a70bbed81266cbd0a8fcb73060
knursultan <knursultana@gmail.com>
2019-12-22 18:22:12 +0600

    389

--- leetcode/389.cpp
+++ leetcode/389.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
-    int letters[30];
+//     int letters[30];
     
-     // O(max(s.size(), t.size())) времени и памяти 
+//      // O(max(s.size(), t.size())) времени и памяти 
+//     char findTheDifference(string s, string t) {
+//         for (int i = 0; i < s.size(); i++)
+//             letters[s[i] - 'a']++;
+//         for (int i = 0; i < t.size(); i++)
+//             letters[t[i] - 'a']++;
+//         for (int i = 0; i < 30; i++)
+//             if (letters[i] % 2 == 1)
+//                 return char(i + 'a');
+//         return 'a';
+//     }
+    
+    
+    int cnt = 0;
     char findTheDifference(string s, string t) {
         for (int i = 0; i < s.size(); i++)
-            letters[s[i] - 'a']++;
-        for (int i = 0; i < t.size(); i++)
-            letters[t[i] - 'a']++;
-        for (int i = 0; i < 30; i++)
-            if (letters[i] % 2 == 1)
-                return char(i + 'a');
-        return 'a';
+            cnt = cnt^(t[i] - 'a')^(s[i] - 'a');
+        cnt = cnt^(t[t.size() - 1] - 'a');
+        return char(cnt + 'a');
     }
 };
