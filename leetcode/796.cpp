1b5cda71e80edd312727d96f366d6d7b020d6651
knursultan <knursultana@gmail.com>
2019-12-29 22:03:38 +0500

    796

--- leetcode/796.cpp
+++ leetcode/796.cpp
@@ -1,4 +1,29 @@
+// время и память O(A.size() + B.size())
+
 class Solution {
+public:
+    bool rotateString(string A, string B) {
+        if (A.size() != B.size())
+            return false;
+        string b = B;
+        B = B + "#" + A + A;
+        vector<int> pi(B.size(), 0);
+        for (int i = 1; i < B.size(); i++) {
+            int j = pi[i - 1];
+            while (j > 0 && B[i] != B[j])
+                j = pi[j - 1];
+            
+            pi[i] = (B[i] == B[j]) ? j + 1 : j;                
+        }
+        
+        for (int i = 0; i < pi.size(); i++)
+            if (pi[i] == b.size())
+                return true;
+        return false;
+    }
+};
+
+/* class Solution {
 public:
     bool rotateString(string A, string B) {
         string rotate = A;
@@ -12,4 +37,4 @@ public:
         }
         return true;
     }
-};
+}; */
--- main.cpp
+++ main.cpp
@@ -1,81 +1,39 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+vector<int> prefix_function(const string& s) {
+    vector<int> pi(s.length(), 0);
 
+    for (int i = 1; i < s.length(); i++) {
+        int j = pi[i - 1];
+
+        while (j > 0 && s[i] != s[j]) {
+            j = pi[j - 1];
+        }
+
+        if (s[i] == s[j]) {
+            pi[i] = j + 1;
+        } else {
+            pi[i] = j;
+        }
+    }
+
+    return pi;
+}
 
 int main() {
-	int n;
-	cin >> n;
-	int a[n + 5][n + 5];
+    string s, t;
+    cin >> s >> t;
+	
+    vector<int> pi = prefix_function(t + '#' + s);
 
-	int left = 0;
-	int down = 0;
-	int right = 1;
-	int up = 0;
-	
-	int i = 1;
-	int j = 1;
-	
-	int x = 1;
-	
-	for (int q = 0; q < n + 5; q++) {
-		for (int t = 0; t < n + 5; t++)
-			a[q][t] = 0;
-	}
-	
-	while (true) {
-		a[i][j] = x++;
-		if (right == 1) {
-			if (a[i][j + 1] == 0 && j + 1 < n + 1) {
-				j++;
-			} else {
-				i++;
-				right = 0;
-				down = 1;
-				left = 0;
-				up = 0;
-			}
-		} else if (down == 1) {
-			if (a[i + 1][j] == 0 && i + 1 < n + 1) {
-				i++;
-			} else {
-				j--;
-				down = 0;			
-				left = 1;
-				up = 0;
-				right = 0;
-			}
-		} else if (left == 1) {
-			if (a[i][j - 1] == 0 && j - 1 > 0) {
-				j--;
-			} else {
-				i--;
-				left = 0;
-				right = 0;
-				up = 1;
-				down = 0;
-			}
-		} else if (up == 1) {
-			if (a[i - 1][j] == 0 && i - 1 > 0) {
-				i--;
-			} else {
-				j++;
-				right = 1;
-				up = 0;
-				down = 0;
-				left = 0;
-			}
-		}
-		//cout << "i " << i << " " << "j " << j << endl;
-		if (x > n*n)
-			break;
-	}
-	
-	for (int q = 1; q <= n; q++) {
-		for (int t = 1; t <= n; t++)
-			printf("%d ", a[q][t]);
-		printf("\n");
-	}
-	return 0;	
+    int t_len = t.length();
+
+    for (int i = 0; i < s.length(); i++) {
+        if (pi[t_len + 1 + i] == t_len) {
+        		cout << t_len + 1 + i << " " << t_len << endl;
+            cout << "s[" << i - t_len + 1 << ".." << i << "] = t" << endl;
+        }
+    }
 }
