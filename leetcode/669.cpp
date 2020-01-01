3f3609e094f3ed5dc1af54aad2154f94419e0b69
knursultan <knursultana@gmail.com>
2020-01-01 13:25:12 +0500

    669

--- leetcode/669.cpp
+++ leetcode/669.cpp
@@ -10,6 +10,7 @@
 
 
 // время и память O(n)
+/*
 class Solution {
 public:
     TreeNode* trimBST(TreeNode* root, int L, int R) {
@@ -18,6 +19,32 @@ public:
         if (root->val < L) return trimBST(root->right, L, R);
         root->left = trimBST(root->left, L, R);
         root->right = trimBST(root->right, L, R);
+        return root;
+    }
+};
+*/
+
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* root, int L, int R) {
+        if (root == NULL) return root;
+        TreeNode* mainRoot = root;
+        while (root->val != L) {
+            if (root->left != NULL)
+                root = root->left;
+            else
+                break;
+        }
+        root->left = NULL;
+        root = mainRoot;
+        while (root->val != R) {
+            if (root->right != NULL)
+                root = root->right;
+            else
+                break;
+        }
+        root->right = NULL;
+        root = mainRoot;
         return root;
     }
 };
